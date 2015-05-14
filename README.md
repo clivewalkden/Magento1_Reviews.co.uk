fa3b3e19c6102cd4bbe43e59973e59e2e7d2b53f
Liam <liam@simplywebsites.com>
2015-05-14 15:31:00 +0100

    Magento Extension

---
+++ README.md
@@ -0,0 +1,21 @@
+# Reviews.co.uk Magento Extension
+
+## Development Installation
+
+- Clone this repo.
+
+- Run setup.sh path/to/magento
+
+- This will create symlinks within magento installation
+
+## Package Extension
+
+- Got to Config > Magento Connect > Package Extensions
+
+- Load Local Package
+
+- Save Data and Create Package
+
+## Adding New Files
+
+If a new file is required. Create the file in this repo, then add a symlink to setup.sh
---
+++ setup.sh
@@ -0,0 +1,7 @@
+ln -s $1/app/code/community/Reviewscouk/ app/code/community/Reviewscouk
+ln -s $1/app/etc/modules/Reviewscouk_Reviews.xml app/etc/modules/Reviewscouk_Reviews.xml
+ln -s $1/app/design/adminhtml/default/default/template/reviews app/design/adminhtml/default/default/template/reviews
+ln -s $1/app/design/adminhtml/default/default/layout/reviews_admin.xml app/design/adminhtml/default/default/layout/reviews_admin.xml
+ln -s $1/app/design/frontend/base/default/layout/reviews_layout.xml app/design/frontend/base/default/layout/reviews_layout.xml
+ln -s $1/app/design/frontend/base/default/template/reviews app/design/frontend/base/default/reviews
+ln -s $1/var/connect/reviewscouk_reviews.xml var/connect/reviewscouk_reviews.xml
