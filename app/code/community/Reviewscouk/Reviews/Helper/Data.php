7d7c296d023f6d488bcf73a63abe5688d439ec90
Liam <liam@simplywebsites.com>
2016-01-14 10:24:01 +0000

    Change Magento Rich Snippet

--- app/code/community/Reviewscouk/Reviews/Helper/Data.php
+++ app/code/community/Reviewscouk/Reviews/Helper/Data.php
@@ -1,4 +1,36 @@
 <?php
 class Reviewscouk_Reviews_Helper_Data extends Mage_Core_Helper_Abstract {
+    public function getRichSnippet($sku=''){
+        $cache = Mage::app()->getCache();
 
+		$apikey = Mage::getStoreConfig('reviewscouk_reviews_settings/api/reviews_api_key', Mage::app()->getStore());
+		$region = Mage::getStoreConfig('reviewscouk_reviews_settings/api/reviews_region', Mage::app()->getStore());
+		$storeName = Mage::getStoreConfig('reviewscouk_reviews_settings/api/reviews_store_id', Mage::app()->getStore());
+        $url = $region == 'us'? 'http://dash.reviews.io/external/rich-snippet/'.$storeName : 'http://dash.reviews.co.uk/external/rich-snippet/'.$storeName;
+
+        if(!empty($sku)){
+            $url = $region == 'us'? 'http://dash.reviews.io/external/rich-snippet/'.$storeName.'?sku='.$sku : 'http://dash.reviews.co.uk/external/rich-snippet/'.$storeName.'?sku='.$sku;
+        }
+
+        $output = '';
+
+        try {
+            $cacheKey = 'rich_snippet_'.$sku;
+            $output = $cache->load($cacheKey);
+            if(!$output){
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL,$url);
+                curl_setopt($ch, CURLOPT_TIMEOUT, 1);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                $output = curl_exec ($ch);
+                curl_close ($ch);
+
+                $cache->save($output, $cacheKey, array("reviews_cache"), 60*60);
+            }
+        }
+        catch(Exception $e){
+        }
+
+        return $output;
+    }
 }
