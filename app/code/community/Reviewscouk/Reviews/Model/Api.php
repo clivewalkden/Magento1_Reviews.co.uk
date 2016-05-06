3c0b394e11887aaa874d78546eef08f02ef05c02
Tom Foyster <tomfoyster@gmail.com>
2016-05-06 13:39:51 +0100

    Added API models for Merchant Reviews

---
+++ app/code/community/Reviewscouk/Reviews/Model/Api.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Class Reviewscouk_Reviews_Model_Api
+ * TODO:- Break this apart into an Abstract function
+ */
+class Reviewscouk_Reviews_Model_Api extends Mage_Core_Model_Abstract
+{
+
+    const XML_CONFIG_REVIEWSCOUK_API_KEY = 'reviewscouk_reviews_settings/api/reviews_api_key';
+    const XML_CONFIG_REVIEWSCOUK_REGION = 'reviewscouk_reviews_settings/api/reviews_region';
+    const XML_CONFIG_REVIEWSCOUK_STORE_NAME = 'reviewscouk_reviews_settings/api/reviews_store_id';
+    const API_URL_MERCHANT_REVIEWS = 'https://api.reviews.co.uk/merchant/reviews';
+    const API_URL_PRODUCT_REVIEWS = 'https://api.reviews.co.uk/product/review';
+
+    /*
+     * Factory API methods
+     */
+    protected function processCurl($url, $params)
+    {
+
+        $paramStrings = array();
+
+        foreach ($params as $param => $value) {
+            $paramStrings[] = $param . '=' . $value;
+        }
+
+        $queryString = implode('&', $paramStrings);
+
+        $ch = curl_init($url . "?" . $queryString);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'store: ' . $this->getApiStoreName(),
+            'apikey: ' . $this->getApiKey()
+        ));
+
+        $data = curl_exec($ch);
+        return $data;
+    }
+
+
+    /*
+     * Store Config Fetch functions
+     */
+    /**
+     * @return mixed
+     */
+    public function getApiKey()
+    {
+        return $apikey = Mage::getStoreConfig(self::XML_CONFIG_REVIEWSCOUK_API_KEY, Mage::app()->getStore());
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getApiRegion()
+    {
+        return $apikey = Mage::getStoreConfig(self::XML_CONFIG_REVIEWSCOUK_REGION, Mage::app()->getStore());
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getApiStoreName()
+    {
+        return $apikey = Mage::getStoreConfig(self::XML_CONFIG_REVIEWSCOUK_STORE_NAME, Mage::app()->getStore());
+    }
+
+}
---
+++ app/code/community/Reviewscouk/Reviews/Model/Api/Merchant.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Class Reviewscouk_Reviews_Model_Api_Merchant
+ */
+class Reviewscouk_Reviews_Model_Api_Merchant extends Reviewscouk_Reviews_Model_Api
+{
+
+    protected $_totalReviews;
+    protected $_totalPages;
+    protected $_averageRating;
+
+    public function __construct()
+    {
+        $this->_totalPages = 0;
+        $this->_totalReviews = 0;
+        $this->_averageRating = 0;
+    }
+
+    public function getMerchantReviews()
+    {
+
+        $data = $this->curlMerchantReviews();
+
+        $this->_totalPages = $data->total_pages;
+        $this->_totalReviews = $data->stats->total_reviews;
+        $this->_averageRating = $data->stats->average_rating;
+
+        return $data->reviews;
+    }
+
+    public function getAverageRating()
+    {
+        return $this->_averageRating;
+    }
+
+    public function getTotalPages()
+    {
+        return $this->_totalPages;
+    }
+
+    public function getTotalReviews()
+    {
+        return $this->_totalReviews;
+    }
+
+    /*
+     * Factory API methods
+     */
+    protected function curlMerchantReviews()
+    {
+
+        $url = self::API_URL_MERCHANT_REVIEWS;
+        $params = array(
+            'per_page' => 25,
+            'include_replies' => 1,
+            'order' => 'desc'
+        );
+
+        $data = $this->processCurl($url, $params);
+        return json_decode($data);
+    }
+
+}
