85e4b0fbbdf21f600a1f66415451c143bfb9c17e
Liam <liamk15@gmail.com>
2017-04-12 11:18:27 +0100

    Hide invisible products when using grouped product option

--- app/code/community/Reviewscouk/Reviews/controllers/IndexController.php
+++ app/code/community/Reviewscouk/Reviews/controllers/IndexController.php
@@ -16,7 +16,11 @@ class Reviewscouk_Reviews_IndexController extends Mage_Core_Controller_Front_Act
 	 */
 	public function feedAction()
 	{
+        $this->helper = Mage::helper('reviewshelper');
+
 		$productFeedEnabled = Mage::getStoreConfig('reviewscouk_reviews_settings/product_feed/product_feed');
+        $useGrouped = $this->helper->config('advanced/reviews_use_group_product_sku', Mage::app()->getStore());
+
 		if ($productFeedEnabled)
 		{
 			$cache = Mage::app()->getCache();
@@ -34,57 +38,61 @@ class Reviewscouk_Reviews_IndexController extends Mage_Core_Controller_Front_Act
 				$products = Mage::getModel('catalog/product')->getCollection();
 				foreach ($products as $prod)
 				{
-					$product = Mage::getModel('catalog/product')->load($prod->getId());
 
-					$brand = $product->getAttributeText('manufacturer') ? $product->getAttributeText('manufacturer') : 'Not Available';
+                    $product = Mage::getModel('catalog/product')->load($prod->getId());
 
-					$price      = $product->getPrice();
-					$finalPrice = $product->getFinalPrice();
+                    $brand = $product->getAttributeText('manufacturer') ? $product->getAttributeText('manufacturer') : 'Not Available';
 
-					$productFeed .= "<item>
-							<g:id><![CDATA[" . $product->getSku() . "]]></g:id>
-							<title><![CDATA[" . $product->getName() . "]]></title>
-							<link>" . $product->getProductUrl() . "</link>
-							<g:price>" . number_format($price, 2) . " " . Mage::app()->getStore()->getDefaultCurrencyCode() . "</g:price>
-							<g:sale_price>" . number_format($finalPrice, 2) . " " . Mage::app()->getStore()->getDefaultCurrencyCode() . "</g:sale_price>
-							<description><![CDATA[" . $product->getDescription() . "]]></description>
-							<g:condition>new</g:condition>
-							<g:image_link>" . $product->getImageUrl() . "</g:image_link>
-							<g:brand><![CDATA[" . $brand . "]]></g:brand>
-							<g:mpn><![CDATA[" . $product->getSku() . "]]></g:mpn>
-							<g:gtin><![CDATA[" . $product->getGtin() . "]]></g:gtin>
-							<g:barcode><![CDATA[" . $product->getBarcode() . "]]></g:barcode>
-							<g:ean><![CDATA[" . $product->getEan() . "]]></g:ean>
-							<g:upc><![CDATA[" . $product->getUpc() . "]]></g:upc>
-							<g:google_product_category><![CDATA[" . $product->getGoogleProductCategory() . "]]></g:google_product_category>
-							<g:product_type><![CDATA[" . $product->getTypeID() . "]]></g:product_type>
-							";
+                    $price      = $product->getPrice();
+                    $finalPrice = $product->getFinalPrice();
 
-					$categoryCollection = $product->getCategoryCollection();
-					if (count($categoryCollection) > 0)
-					{
-						foreach ($categoryCollection as $category)
-						{
-							$productFeed .= "<g:google_product_category><![CDATA[" . $category->getName() . "]]></g:google_product_category>";
-						}
-					}
+                    // If using grouped, don't show invisible products
+                    if(!$useGrouped || $product->getVisibility() > 1){
+                        $productFeed .= "<item>
+                        <g:id><![CDATA[" . $product->getSku() . "]]></g:id>
+                        <title><![CDATA[" . $product->getName() . "]]></title>
+                        <link>" . $product->getProductUrl() . "</link>
+                        <g:price>" . number_format($price, 2) . " " . Mage::app()->getStore()->getDefaultCurrencyCode() . "</g:price>
+                        <g:sale_price>" . number_format($finalPrice, 2) . " " . Mage::app()->getStore()->getDefaultCurrencyCode() . "</g:sale_price>
+                        <description><![CDATA[" . $product->getDescription() . "]]></description>
+                        <g:condition>new</g:condition>
+                        <g:image_link>" . $product->getImageUrl() . "</g:image_link>
+                        <g:brand><![CDATA[" . $brand . "]]></g:brand>
+                        <g:mpn><![CDATA[" . $product->getSku() . "]]></g:mpn>
+                        <g:gtin><![CDATA[" . $product->getGtin() . "]]></g:gtin>
+                        <g:barcode><![CDATA[" . $product->getBarcode() . "]]></g:barcode>
+                        <g:ean><![CDATA[" . $product->getEan() . "]]></g:ean>
+                        <g:upc><![CDATA[" . $product->getUpc() . "]]></g:upc>
+                        <g:google_product_category><![CDATA[" . $product->getGoogleProductCategory() . "]]></g:google_product_category>
+                        <g:product_type><![CDATA[" . $product->getTypeID() . "]]></g:product_type>
+                        ";
 
-					$stock = $product->getStockItem();
-					if ($stock->getIsInStock())
-					{
-						$productFeed .= "<g:availability>in stock</g:availability>";
-					}
-					else
-					{
-						$productFeed .= "<g:availability>out of stock</g:availability>";
-					}
+                        $categoryCollection = $product->getCategoryCollection();
+                        if (count($categoryCollection) > 0)
+                        {
+                            foreach ($categoryCollection as $category)
+                            {
+                                $productFeed .= "<g:google_product_category><![CDATA[" . $category->getName() . "]]></g:google_product_category>";
+                            }
+                        }
 
-					$productFeed .= "</item>";
+                        $stock = $product->getStockItem();
+                        if ($stock->getIsInStock())
+                        {
+                            $productFeed .= "<g:availability>in stock</g:availability>";
+                        }
+                        else
+                        {
+                            $productFeed .= "<g:availability>out of stock</g:availability>";
+                        }
+
+                        $productFeed .= "</item>";
+                    }
 				}
 
 				$productFeed .= "</channel></rss>";
 
-				$cache->save($productFeed, "feed", array("nicks_cache"), 86400);
+				$cache->save($productFeed, "feed", array("reviews_cache"), 86400);
 			}
 			else
 			{
